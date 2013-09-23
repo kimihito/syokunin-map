32aadcc49dd2467105ff86ade6cf03fb4a405b3b
tompng <tomoyapenguin@gmail.com>
2013-09-23 16:57:23 +0900

    indexmap foooooooooooooooooooooooooooooo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SyokuninMap::Application.routes.draw do
+  get '/', to: 'index#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
