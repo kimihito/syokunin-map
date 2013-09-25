0af871f641ac0497addd35d33eac1c617a01800f
tompng <tomoyapenguin@gmail.com>
2013-09-25 22:16:09 +0900

    assets problem fix

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module SyokuninMap
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += ['*.svg']
     config.secret_key_base='hoge'
   end
 end
